e8997a8fac1292b71f70168b80d46fa72c782c45
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-12-10 23:30:30 +0800

    js\jquery3.3.1.js

--- js/jquery3.3.1.js
+++ js/jquery3.3.1.js
@@ -10393,24 +10393,24 @@ $(function(){
 
 		},
 		complete: function(XMLHttpRequest, textStatus) {
-			$("#myMaskDiv").hide();
+			$("#myMaskDiv").hide().remove();
 		},
 		error: function(jqXHR, exception) {
 			var msg = "";
 			if (jqXHR.status === 0) {
-			msg = "Not connect.\n Verify Network.";
+				msg = "Not connect.\n Verify Network.";
 			} else if (jqXHR.status == 404) {
-			msg = "Requested page not found. [404]";
+				msg = "Requested page not found. [404]";
 			} else if (jqXHR.status == 500) {
-			msg = "Internal Server Error [500].";
+				msg = "Internal Server Error [500].";
 			} else if (exception === "parsererror") {
-			msg = "Requested JSON parse failed.";
+				msg = "Requested JSON parse failed.";
 			} else if (exception === "timeout") {
-			msg = "Time out error.";
+				msg = "Time out error.";
 			} else if (exception === "abort") {
-			msg = "Ajax request aborted.";
+				msg = "Ajax request aborted.";
 			} else {
-			msg = "Uncaught Error.\n" + jqXHR.responseText;
+				msg = "Uncaught Error.\n" + jqXHR.responseText;
 			}
 			console.log('%c%s', 'color: red' , exception);
 			alert(msg);
